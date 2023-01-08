5180af9ae7b6cd3eca39b798daac11431925ef62
Петр Кравцов <rat.f.br@gmail.com>
2023-01-08 09:51:29 +0300

    drf throttling added

--- orders/settings.py
+++ orders/settings.py
@@ -99,6 +99,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/hour',
+        'user': '100/hour'
+    }
 }
 
 AUTH_USER_MODEL = 'backend.User'
